f403ebae61e8eefeb8586e3fedb450f4614e43f9
jakubjamielucha <jakubjamielucha@gmail.com>
2023-11-10 00:15:10 +0100

    Changing the layout of the Main Activity and creating Game Activity.

--- app/src/main/java/com/example/mathgame/MainActivity.java
+++ app/src/main/java/com/example/mathgame/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.mathgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addition, subtraction, multiplication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addition = findViewById(R.id.buttonAddition);
+        subtraction = findViewById(R.id.buttonSubtraction);
+        multiplication = findViewById(R.id.buttonMultiplication);
+
+        addition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, Game.class);
+                startActivity(intent);
+            }
+        });
     }
 }
